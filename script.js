85b43097b444fe4b0f6083fd19c4bf3941b5a98d
Ganeshbabu7 <110327597+Ganeshbabu7@users.noreply.github.com>
2022-09-23 17:37:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1. Difference Between HTTP1.1 vs HTTP 2
+	
+// 	HTTP : Hypertext transfer protocol
+//  HTTP2 is much faster than HTTP1.1 
+// 	Now a days we are mostly using Https ( s indicates secured website).
+
+// 2. Objects and its Internal representation
+
+//     1) Objects are notified inside curly brackets { }
+//     2)Inside the Object it contains Keys and values
+//     3) Object will be defined by using variable for eg : let object = { }
+//     4) And inside one object we can use N no. of objects that will be called as nested objects
+//     5) Objects will be print by using two methods 
+//     6) Dot notation.    
+//     7) Bracket notation.
+//     8) We can use objects in forms to get the data from user 
+
+// Object Syntax :
+
+// Let a = {
+// 	Key1 : "Value1",
+// 	Key2 : "Value3",
+// Key3 : "Value3",
+// Key4 : {
+// 	Key 1 : "Value1",
+// }
+// }
+
+// Dot Notation : Console.log (a.key1)
+// Bracket Notation : Console.log (a [‘key1’])
